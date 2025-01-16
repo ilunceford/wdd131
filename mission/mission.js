76084c7b66cae6014a54d73113d7d2cc692bc243
ilunceford <lun23014@byui.edu>
2025-01-16 15:26:54 -0700

    mission project completed

--- mission/mission.js
+++ mission/mission.js
@@ -1,24 +1,15 @@
-// Select the <select> element from the DOM
-const themeSelector = document.querySelector('#themeSelector');
+const themeSelector = document.querySelector('#mode-select');
 
-// Define the changeTheme function
 function changeTheme() {
-    // Check what option is currently selected in the theme selector
     const currentTheme = themeSelector.value;
 
-    // If the selected option is "dark"
     if (currentTheme === 'dark') {
-        // Add the dark class to the body
         document.body.classList.add('dark');
-        // Change the logo image src to the white logo
-        document.querySelector('#logo').src = 'white-logo.png';
+        document.querySelector('#logo').src = 'byui-logo_white.png';
     } else {
-        // If it's not "dark", remove the dark class from the body
         document.body.classList.remove('dark');
-        // Change the logo image src to the blue logo
-        document.querySelector('#logo').src = 'blue-logo.png';
+        document.querySelector('#logo').src = 'byui-logo_blue.webp';
     }
 }
 
-// Add an event listener to the themeSelector element
 themeSelector.addEventListener('change', changeTheme);
--- ponder/dom-basics.js
+++ ponder/dom-basics.js
@@ -3,10 +3,25 @@ newP.textContent = "hello, world!";
 document.body.append(newP);
 
 const newI = document.createElement("img")
-newI.setAttribute ("src", "https://picsum.photos/200");
-newI.setAttribute ("alt", "new photo")
+newI.setAttribute("src", "https://picsum.photos/200");
+newI.setAttribute("alt", "new photo")
 document.body.appendChild(newI);
 
 const newSection = document.createElement("section");
 newSection.innerHTML = "<h2>DOM Basics</h2> <p>This was added through Javascript</p>";
 document.body.append(newSection);
+
+const ingredientData = ['Pinto Beans', 'Corn', 'Spices', 'Tortillas'];
+const portionData = ['1 15oz can', '1 15oz can', '1 tbsp', '8'];
+
+function ingredientTemplate(index) {
+    return `<li>${portionData[0]} ${ingredientData[0]}</li>`
+}
+const myList = document.createElement("ul");
+ingredientData.forEach(function(item, index) { 
+    myList.innerHTML += ingredientTemplate(index);
+})
+
+document.body.append(myList);
+
+myList.classList.add("dark")
